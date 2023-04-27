ad6e962b1c22e07e87e55e09cb59b7be12f21ff5
Mateo Gonzalez Cosio <mateoglzc@hotmail.com>
2023-04-26 18:45:51 -0600

    Moved preprocesing logic to another file

--- main.py
+++ main.py
@@ -1,32 +1,6 @@
 # Explicit Semantic Analysis Test
 import os
 
-def analyze_text(path: str):
-    """
-    Given a input path of a .txt file, return a bag of words vector of that given file and the amount of words that file contains.
-    """
-
-    # Get the extension of a file
-    ext = os.path.splitext(path)[-1].lower()
-
-    if ext != ".txt":
-        raise Exception("Input file is not a text file")
-
-    bag_words = {}
-    amount_words = 0
-
-    with open(path, "r") as f:
-        lines = f.readlines()
-        for line in lines:
-            # This split is not efficient enough. It will be better if we can omit punctuation and consider parethesis as one word
-            words = line.split()
-            for word in words:
-                w = word.lower()
-                bag_words[w] = bag_words.get(w, 0) + 1
-                amount_words += 1
-
-    return amount_words, bag_words
-
 def compare_texts(inverted_index: InvertedIndex, text: list) -> float:
     pass
 
---
+++ preprocesing.py
@@ -0,0 +1,31 @@
+def analyze_text(path: str):
+    """
+    Given a input path of a .txt file, return a bag of words vector of that given file and the amount of words that file contains.
+    """
+
+    # Get the extension of a file
+    ext = os.path.splitext(path)[-1].lower()
+
+    if ext != ".txt":
+        raise Exception("Input file is not a text file")
+
+    bag_words = {}
+    amount_words = 0
+
+    with open(path, "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            # This split is not efficient enough. It will be better if we can omit punctuation and consider parethesis as one word
+            words = line.split()
+            for word in words:
+                w = word.lower()
+                bag_words[w] = bag_words.get(w, 0) + 1
+                amount_words += 1
+
+    return amount_words, bag_words
+
+def preprocesing():
+    pass
+
+if __name__ == "__main__":
+    preprocesing()
