419c3ed6b77c6136a6accf847b0187752a2af63a
Sebastian Juncos <sebastianjuncos@Sebastians-MacBook-Pro.local>
2023-04-28 17:03:58 -0600

    Added base for padding the data

--- ngrams.py
+++ ngrams.py
@@ -1,10 +1,10 @@
 import re
 
-from nltk.util import ngrams, pad_sequence, everygrams
-from nltk.tokenize import word_tokenize
-from nltk.lm import MLE, WittenBellInterpolated
+# from nltk.util import ngrams, pad_sequence, everygrams
+# from nltk.tokenize import word_tokenize
+# from nltk.lm import MLE, WittenBellInterpolated
 
-import numpy as np
+# import numpy as np
 
 # N-grams
 
@@ -14,7 +14,22 @@ import numpy as np
 # 3.- Generate n-grams from the training data
 # 4.- Fit the model
 
-def pad_data(tokenize_data: list) -> list:
+def pad_data(full_text: str) -> list:
+    # Separate text into sentences
+    sentences = full_text.split(". ")
+    # Remove dot at the end of the sentences (if needed)
+    sentences = [s.strip(".") for s in sentences]
+    sentences = [s.strip("./n") for s in sentences]
+    pad_list = []
+    # print(sentences)
+    # This for loop will add an <s> before the start of each sentence
+    # and a <\s> after each sentence
+    for i in sentences:
+        pad_list.append("<s>")
+        splitted_sentence = i.split()
+        pad_list.extend(splitted_sentence)
+        pad_list.append("<\s>")
+    
     """
     Pad the training data by sentences.
     Ex.
@@ -26,7 +41,7 @@ def pad_data(tokenize_data: list) -> list:
     <s> -> start sentence
     <\s> -> end sentence
     """
-    pass
+    print(pad_list)
 
 
 
@@ -38,52 +53,54 @@ if __name__ == "__main__":
 
     with open(training_data_path, "r") as f:
         train_text = f.read().lower()
+    
+    pad_data(train_text)
 
-    # Remove punctuation
+    # # Remove punctuation
      
-    train_text = re.sub(r"\[.*\]|\{.*\}", "", train_text)
-    train_text = re.sub(r'[^\w\s]', "", train_text)
+    # train_text = re.sub(r"\[.*\]|\{.*\}", "", train_text)
+    # train_text = re.sub(r'[^\w\s]', "", train_text)
 
-    # N-Gram number
-    N = 4
+    # # N-Gram number
+    # N = 4
 
-    # Pad Text (Basically just insert '<s>' in the beginning)
-    training_data = list(pad_sequence(word_tokenize(train_text), N, 
-                                  pad_left=True, 
-                                  left_pad_symbol="<s>"))
+    # # Pad Text (Basically just insert '<s>' in the beginning)
+    # training_data = list(pad_sequence(word_tokenize(train_text), N, 
+    #                               pad_left=True, 
+    #                               left_pad_symbol="<s>"))
 
-    print(f"Training data: {training_data}\n")
+    # print(f"Training data: {training_data}\n")
 
-    # Generate ngrams
-    ngrams = list(everygrams(training_data, max_len=N))
+    # # Generate ngrams
+    # ngrams = list(everygrams(training_data, max_len=N))
 
-    print("Number of ngrams:", len(ngrams))
+    # print("Number of ngrams:", len(ngrams))
 
-    # Build ngram language models
-    model = WittenBellInterpolated(N) # Quien sabe que tipo de modelo es este?
-    model.fit([ngrams], vocabulary_text=training_data)
-    print(model.__dict__)
+    # # Build ngram language models
+    # model = WittenBellInterpolated(N) # Quien sabe que tipo de modelo es este?
+    # model.fit([ngrams], vocabulary_text=training_data)
+    # print(model.__dict__)
 
-    # testing data file
-    test_data_file = "text_two.txt"
+    # # testing data file
+    # test_data_file = "text_two.txt"
 
-    # Read testing data
-    with open(test_data_file) as f:
-        test_text = f.read().lower()
-    test_text = re.sub(r'[^\w\s]', "", test_text)
+    # # Read testing data
+    # with open(test_data_file) as f:
+    #     test_text = f.read().lower()
+    # test_text = re.sub(r'[^\w\s]', "", test_text)
 
-    # Tokenize and pad the text
-    testing_data = list(pad_sequence(word_tokenize(test_text), N, 
-                                    pad_left=True,
-                                    left_pad_symbol="<s>"))
-    print("Length of test data:", len(testing_data))
+    # # Tokenize and pad the text
+    # testing_data = list(pad_sequence(word_tokenize(test_text), N, 
+    #                                 pad_left=True,
+    #                                 left_pad_symbol="<s>"))
+    # print("Length of test data:", len(testing_data))
 
-    # assign scores
-    scores = []
-    for i, item in enumerate(testing_data[N-1:]):                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
-        s = model.score(item, testing_data[i:i+N-1])
-        print(f"Predictability: {round(s, 2):4} Word: '{item}'")
-        scores.append(s)
+    # # assign scores
+    # scores = []
+    # for i, item in enumerate(testing_data[N-1:]):
+    #     s = model.score(item, testing_data[i:i+N-1])
+    #     print(f"Predictability: {round(s, 2):4} Word: '{item}'")
+    #     scores.append(s)
 
-    print(f"avg: {sum(scores)/len(scores)}")
+    # print(f"avg: {sum(scores)/len(scores)}")
 
