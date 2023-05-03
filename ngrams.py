61001742d0d786be9e7e29a60022f0c1b4f9fb63
Mateo Gonzalez Cosio <mateoglzc@hotmail.com>
2023-05-02 20:44:53 -0600

    Ngram preprocesing complete

--- ngrams.py
+++ ngrams.py
@@ -1,9 +1,8 @@
 import re
-import string
 
-# from nltk.util import ngrams, pad_sequence, everygrams
-# from nltk.tokenize import word_tokenize
-# from nltk.lm import MLE, WittenBellInterpolated
+from nltk.util import ngrams, pad_sequence, everygrams
+from nltk.tokenize import word_tokenize
+from nltk.lm import MLE, WittenBellInterpolated
 
 # import numpy as np
 
@@ -16,42 +15,24 @@ import string
 # 4.- Fit the model
 
 def pad_data(full_text: str) -> list:
+    """
+    Pad the training data by sentences.
+    """
 
     # Separate text into sentences
     sentences = full_text.split(". ")
-    # Remove dot at the end of the sentences (if needed)
     sentences = [s.strip(".") for s in sentences]
     sentences = [s.strip("./n") for s in sentences]
     pad_list = []
-    # print(sentences)
-    # This for loop will add an <s> before the start of each sentence
-    # and a <\s> after each sentence
     for i in sentences:
         pad_list.append("<s>")
         splitted_sentence = i.split()
         pad_list.extend(splitted_sentence)
         pad_list.append("<\s>")
-    
-    """
-    Pad the training data by sentences.
-    Ex.
-    i am Sam.
-    Sam am i.
-    i don't like green eggs and ham.
-    ->
-    [<s>, i, am, sam, <\s>, <s>, sam, i, am, <\s>, i, don't, like, green, eggs, and, ham, <\s>]
-    <s> -> start sentence
-    <\s> -> end sentence
-    """
-    
-    # Separate text into sentences
     sentences = full_text.split(". ")
-    # Remove dot at the end of the sentences (if needed)
     sentences = [s.strip(".") for s in sentences]
     sentences = [s.strip("./n") for s in sentences]
     pad_list = []
-    # This for loop will add an <s> before the start of each sentence
-    # and a <\s> after each sentence
     for i in sentences:
         pad_list.append("<s>")
         splitted_sentence = i.split()
@@ -64,18 +45,14 @@ def pad_data(full_text: str) -> list:
 def group_creation(pad_list: list):
     grouped_list = []
     for i in range(len(pad_list)-1):
-        # if(i == 0):
-        #     pair = (pad_list[0],)
-        #     grouped_list.append(pair)
         pair = (pad_list[i],)
         grouped_list.append(pair)
         pair = (pad_list[i], pad_list[i+1])
         grouped_list.append(pair)
     pair = (pad_list[-1],)
     grouped_list.append(pair)
-    print(grouped_list)
     
-    # return grouped_list
+    return grouped_list
 
 def markov_assumption(ngrams_set: list, ngram: int):
     pass 
@@ -104,56 +81,38 @@ if __name__ == "__main__":
     with open(training_data_path, "r") as f:
         train_text = f.read().lower()
     
-    pad_d = pad_data(train_text)
+    training_data = pad_data(train_text)
 
-    # print(f"Our padded data: {pad_d}")
-    group_creation(pad_d)
+    print(f"Our padded data: {training_data}")
 
-    # # Remove punctuation
-     
-    # train_text = re.sub(r"\[.*\]|\{.*\}", "", train_text)
-    # train_text = re.sub(r'[^\w\s]', "", train_text)
+    ngrams = group_creation(training_data)
+
+    print(f"N-grams: {ngrams}")
 
     # N-Gram number
-    # N = 1
+    N = 2
 
-    # # Pad Text (Basically just insert '<s>' in the beginning)
-    # training_data = list(pad_sequence(word_tokenize(train_text), N, 
-    #                               pad_left=True, 
-    #                               left_pad_symbol="<s>"))
+    # Build ngram language models
+    model = WittenBellInterpolated(N) 
+    model.fit([ngrams], vocabulary_text=training_data)
 
-    # print(f"Training data: {training_data}\n")
+    test_data_file = "./text_two.txt"
 
-    # # Generate ngrams
-    # ngrams = list(everygrams(training_data, max_len=N))
+    # Read testing data
+    with open(test_data_file) as f:
+        test_text = f.read().lower()
+    test_text = re.sub(r'[^\w\s]', "", test_text)
 
-    # print(f"Ngram-ed data: {ngrams}")
+    # Tokenize and pad the text
+    testing_data = pad_data(test_text)
+    print(f"Testing data: {testing_data}")
 
-    # # Build ngram language models
-    # model = WittenBellInterpolated(N) # Quien sabe que tipo de modelo es este?
-    # model.fit([ngrams], vocabulary_text=training_data)
-    # print(model.__dict__)
+    # assign scores
+    scores = []
+    for i, item in enumerate(testing_data[N-1:]):
+        s = model.score(item, testing_data[i:i+N-1])
+        print(f"Predictability: {round(s, 2):4} Word: '{item}'")
+        scores.append(s)
 
-    # # testing data file
-    # test_data_file = "text_two.txt"
-
-    # # Read testing data
-    # with open(test_data_file) as f:
-    #     test_text = f.read().lower()
-    # test_text = re.sub(r'[^\w\s]', "", test_text)
-
-    # # Tokenize and pad the text
-    # testing_data = list(pad_sequence(word_tokenize(test_text), N, 
-    #                                 pad_left=True,
-    #                                 left_pad_symbol="<s>"))
-    # print("Length of test data:", len(testing_data))
-
-    # # assign scores
-    # scores = []
-    # for i, item in enumerate(testing_data[N-1:]):
-    #     s = model.score(item, testing_data[i:i+N-1])
-    #     print(f"Predictability: {round(s, 2):4} Word: '{item}'")
-    #     scores.append(s)
-
-    # print(f"avg: {sum(scores)/len(scores)}")
+    print(f"avg: {sum(scores)/len(scores)}")
 
